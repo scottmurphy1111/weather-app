7f0078771cc372f52c6250686665bb9b6159c71e
scottmurphy1111 <scottmurphy1111@gmail.com>
2018-06-08 00:11:58 -0400

    get stack over flow data using axios

--- src/components/portfolio/Socials.js
+++ src/components/portfolio/Socials.js
@@ -2,44 +2,58 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export class Socials extends Component {
-    state = { soData: '' }
+    state = { 
+        totalSoRep: '',
+        bronzeBadges: '',
+        silverBadges: '',
+        goldBadges: '',
+        totalSoBadges: ''
+    };
     
     getSoData = () => {
         const url = 'http://api.stackexchange.com/2.2/users/5711949/?site=stackoverflow';
         axios.get(url)
             .then(resp => {
-                this.setState({ soData: resp});
+                console.log(resp)
+                var allItems = resp.data.items[0],
+                badgeCounts = allItems.badge_counts;
+                this.setState({ 
+                    totalSoRep: allItems.reputation,
+                    bronzeBadges: badgeCounts.bronze,
+                    silverBadges: badgeCounts.silver,
+                    goldBadges: badgeCounts.gold,
+                    totalSoBadges: Object.keys(badgeCounts).length
+                });
             }
         );
     }
 
     componentDidMount() {
         this.getSoData();
-        console.log(this.state);
     }
     render() {
         const socials = this.props.content.contact[0].socials.map((social, index) => 
             <li key={index}>
                 <a title={social.title} target="_blank" href={social.link}>
-                    {/* <div className="so-info-wrap">
+                    <div className="so-info-wrap">
                         {(social.title === 'Stack Overflow' ?
                         <div>
                             <div className="so-tooltip">
-                                <p>Reputation: {totalSoRep}</p>
+                                <p>Reputation: {this.state.totalSoRep}</p>
                                 <hr />
                                 <p>Badges:</p>
                                 <ul>
-                                    <li>Bronze: {bronzeBadges}</li>
-                                    <li>Silver: {silverBadges}</li>
-                                    <li>Gold: {goldBadges}</li>
+                                    <li>Bronze: {this.state.bronzeBadges}</li>
+                                    <li>Silver: {this.state.silverBadges}</li>
+                                    <li>Gold: {this.state.goldBadges}</li>
                                 </ul>
                             </div>
-                            <div className="so-info rep">{totalSoRep}</div>
-                            <div className="so-info badges">{totalSoBadges}</div> 
+                            <div className="so-info rep">{this.state.totalSoRep}</div>
+                            <div className="so-info badges">{this.state.totalSoBadges}</div> 
                         </div>
                         : null
                         )}
-                    </div> */}
+                    </div>
                     <img src={social.icon} />
                 </a>
             </li>
