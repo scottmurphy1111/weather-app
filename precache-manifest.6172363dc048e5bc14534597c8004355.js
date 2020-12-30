4b5d1485d0e6505736cb01609ae8491ff112a0b3
scottmurphy1111 <scottmurphy1111@gmail.com>
2020-12-29 20:27:29 -0500

    Updates

---
+++ precache-manifest.6172363dc048e5bc14534597c8004355.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75578295c78c3cbbbbcac67059dd897a",
+    "url": "/sm-port-react/index.html"
+  },
+  {
+    "revision": "37a7c3bed640cb22e0d2",
+    "url": "/sm-port-react/static/css/main.1e5be5d4.chunk.css"
+  },
+  {
+    "revision": "ff5c29162e46c3edbd70",
+    "url": "/sm-port-react/static/js/2.d560b354.chunk.js"
+  },
+  {
+    "revision": "22944df67a1e9cf588daa5255143530c",
+    "url": "/sm-port-react/static/js/2.d560b354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a7c3bed640cb22e0d2",
+    "url": "/sm-port-react/static/js/main.6d9ae976.chunk.js"
+  },
+  {
+    "revision": "2d83528658ea9ed9867f",
+    "url": "/sm-port-react/static/js/runtime-main.4cf05acd.js"
+  },
+  {
+    "revision": "3bb60a8ae87f0819ee4a8641b2720a78",
+    "url": "/sm-port-react/static/media/SourceSansPro-ExtraLight.3bb60a8a.ttf"
+  },
+  {
+    "revision": "b2e90cc01cdd1e2e6f214d5cb2ae5c26",
+    "url": "/sm-port-react/static/media/SourceSansPro-Light.b2e90cc0.ttf"
+  },
+  {
+    "revision": "ba6cad25afe01d394e830f548a7f94df",
+    "url": "/sm-port-react/static/media/SourceSansPro-Regular.ba6cad25.ttf"
+  },
+  {
+    "revision": "603934185ba4500d610a71d6fea6de8d",
+    "url": "/sm-port-react/static/media/bg-pattern-light.60393418.png"
+  },
+  {
+    "revision": "3cf818fefc94072ecc87291972c1acc6",
+    "url": "/sm-port-react/static/media/digi-bg1.3cf818fe.png"
+  },
+  {
+    "revision": "4df0fcdd9e167dce2d38b3216be11db6",
+    "url": "/sm-port-react/static/media/finger-swipe.4df0fcdd.svg"
+  },
+  {
+    "revision": "70c0afdaca489676261d71a99231ae28",
+    "url": "/sm-port-react/static/media/golf-ball.70c0afda.png"
+  },
+  {
+    "revision": "73de59f75c5657cdf6b9471d838641aa",
+    "url": "/sm-port-react/static/media/location.73de59f7.svg"
+  },
+  {
+    "revision": "6a1c5f2105ec16fbafa48e4b047b37dc",
+    "url": "/sm-port-react/static/media/me-trans.6a1c5f21.png"
+  },
+  {
+    "revision": "6e35eb2309d7eea2141ffe32240835e1",
+    "url": "/sm-port-react/static/media/minus.6e35eb23.svg"
+  },
+  {
+    "revision": "a759dea05235a0476512a1606375c911",
+    "url": "/sm-port-react/static/media/plus.a759dea0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.9493a19560d770a53f75bca81dc63ecf.js"
+  "/sm-port-react/precache-manifest.6172363dc048e5bc14534597c8004355.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sm-port-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
