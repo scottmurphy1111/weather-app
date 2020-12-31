c5b72deab36c2dc73fd269002eee502ed96d8524
scottmurphy1111 <scottmurphy1111@gmail.com>
2020-12-30 23:07:21 -0500

    Updates

---
+++ precache-manifest.9493a19560d770a53f75bca81dc63ecf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6626a1bbb33a776f783ba997192fe62",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d1d20f626d265bc738da",
+    "url": "/weather-app/static/css/main.5010355c.chunk.css"
+  },
+  {
+    "revision": "a37f87fbc82d9d18bd9a",
+    "url": "/weather-app/static/js/2.c70d97be.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/weather-app/static/js/2.c70d97be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d20f626d265bc738da",
+    "url": "/weather-app/static/js/main.e9547da4.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "63dde3df34c46650a29046a7a7784548",
+    "url": "/weather-app/static/media/clear.63dde3df.jpg"
+  },
+  {
+    "revision": "b9fa792566057d994c7f0f9e7bb1efa7",
+    "url": "/weather-app/static/media/clouds.b9fa7925.jpg"
+  },
+  {
+    "revision": "6663a8ee2996495ec2160cc84ac9cebd",
+    "url": "/weather-app/static/media/drizzle.6663a8ee.jpg"
+  },
+  {
+    "revision": "8c35925bfe23aef4c0396f5887b1fd7b",
+    "url": "/weather-app/static/media/rain.8c35925b.jpg"
+  },
+  {
+    "revision": "e7c7d9fe05aad20edeb838c5dd34dcef",
+    "url": "/weather-app/static/media/richmond.e7c7d9fe.jpg"
+  },
+  {
+    "revision": "33c898683f42a0dda37f1bac1487a523",
+    "url": "/weather-app/static/media/snow.33c89868.jpg"
+  },
+  {
+    "revision": "4bfeb6da64adcd746ee2ee9df63c4442",
+    "url": "/weather-app/static/media/thunderstorm.4bfeb6da.jpg"
+  },
+  {
+    "revision": "8c1c2adaabd8884e878554cbd82ccf96",
+    "url": "/weather-app/static/media/wind-arrow.8c1c2ada.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sm-port-react/precache-manifest.6172363dc048e5bc14534597c8004355.js"
+  "/weather-app/precache-manifest.9493a19560d770a53f75bca81dc63ecf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sm-port-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
